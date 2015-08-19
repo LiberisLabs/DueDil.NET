0b8c2a295d2c91dc38285411f8148e520a680f59
kev_bite <kev_bite@msn.com>
2015-08-19 17:44:43 +0100

    initial start of DueDil SDK.

---
+++ src/Liberis.DueDil.Sdk.Tests/UriBuilders/SearchCompaniesUriBuilderTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Web;
+using Liberis.DueDil.Sdk.Requests.SearchCompanies;
+using Liberis.DueDil.Sdk.UriBuilders;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Liberis.DueDil.Sdk.Tests.UriBuilders
+{
+    [TestFixture]
+    public class SearchCompaniesUriBuilderTests
+    {
+        private Uri _actualUri;
+        private string _apiKey;
+        private string _name;
+        private Uri _baseUri;
+
+        [SetUp]
+        public void GivenACompanyName_WhenBuildingAUri()
+        {
+            _baseUri = new Uri("wibble://liberisDevs.co.uk/bla");
+            _apiKey = Guid.NewGuid().ToString();
+            _name = "Liberis";
+            var terms = new Terms() {Name = _name};
+            var builder = new SearchCompaniesUriBuilder(_baseUri, _apiKey);
+
+            _actualUri = builder.BuildUri(terms);
+        }
+
+        [Test]
+        public void ThenTheUriSchemaIsFromBaseUri()
+        {
+            Assert.That(_actualUri.Scheme, Is.EqualTo(_baseUri.Scheme));
+        }
+
+        [Test]
+        public void ThenTheUriHostIsFromBaseUri()
+        {
+            Assert.That(_actualUri.Host, Is.EqualTo(_baseUri.Host));
+        }
+
+        [Test]
+        public void ThenTheUriPathIsCorrect()
+        {
+            Assert.That(_actualUri.AbsolutePath, Is.EqualTo("/v3/companies"));
+        }
+
+        [Test]
+        public void ThenTheUriQueryStringContainsApiKey()
+        {
+            var query = HttpUtility.ParseQueryString(_actualUri.Query);
+
+            Assert.That(query["api_key"], Is.EqualTo(_apiKey));
+        }
+
+        [Test]
+        public void ThenTheUriQueryStringContainsTheCorrectFilter()
+        {
+            var query = HttpUtility.ParseQueryString(_actualUri.Query);
+            var filters = JsonConvert.DeserializeObject<JObject>(query["filters"]);
+            var namefilter = filters["name"].Value<string>();
+
+            Assert.That(namefilter, Is.EqualTo(_name));
+        }
+    }
+}
---
+++ src/Liberis.DueDil.Sdk/Responses/Pagination.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liberis.DueDil.Sdk.Responses
+{
+    public class Pagination
+    {
+        [JsonProperty(PropertyName = "previous_url")]
+        public string PreviousUrl { get; set; }
+
+        [JsonProperty(PropertyName = "next_url")]
+        public string NextUrl { get; set; }
+
+        [JsonProperty(PropertyName = "total")]
+        public int Total { get; set; }
+    }
+
+}
